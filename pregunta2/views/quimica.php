6bae129aeb0dabe88a8b9490733d6fabe8d70ae4
kirito-byte <57415531+kirito-byte@users.noreply.github.com>
2022-10-12 09:55:12 -0400

    Add files via upload

---
+++ pregunta2/views/quimica.php
@@ -0,0 +1,49 @@
+<?php include('header.php'); ?>
+<body>
+    <header>
+        <div id="titulo">   
+            <h1>
+                Quimica
+            </h1>
+        </div>
+        <div id="nav">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="<?php echo base_url(); ?>">F.C.P.N</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url(); ?>fisica">fisica</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url(); ?>matematica">Matematica</a>
+                    </li>
+                    
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <section class="principal">
+            <aside>
+                <h4>
+                    MISIÓN
+                </h4>
+                    <p>
+                    La Carrera de Ciencias Químicas de la Universidad Mayor de San Andrés es una institución de educación superior pública y gratuita, que dentro del marco de la Autonomía y el cogobiemo, tiene la misión de formar recursos humanos en Ciencias Químicas de elevada calificación, competitividad profesional, creatividad, adaptación, con valores éticos, la suficiente conciencia social y vocación de servicio para responder a las necesidades de la población, al desarrollo local, regional y nacional.
+                    </p>
+                <br>
+                <h4>
+                    VISIÓN
+                </h4>
+                    <p>
+                    La Carrera de Ciencias Químicas es un referente en la formación de profesionales químicos identificados con su pueblo, con capacidad para encarar problemas diversos referentes a la Química con autoridad científica y suficiente ética. Desarrolla sus actividades académicas, de investigación e interacción social de acuerdo al contexto local, regional, nacional e internacional.
+                    </p>
+            </aside>
+            <article>
+                Aqui va el articulo
+            </article>
+        </section>
+        <?php
+            include ('footer.php');
+        ?>
